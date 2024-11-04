f2ce114448165dd580b7e705e65d1c1e691b26ef
Chufretalas <marcoantonio.bene@gmail.com>
2024-11-04 14:35:53 -0300

    finished the tests for the 1 bit ALU

--- ALU_1_bit_TB.vhd
+++ ALU_1_bit_TB.vhd
@@ -29,86 +29,156 @@ BEGIN
     PROCESS IS
     BEGIN
         ---------------------------- TESTING AND - Operation = OP_AND && A_Invert = 0 && B_Invert = 0 ----------------------------
+        r_Operation <= OP_AND;
+        r_A_Invert <= '0';
+        r_B_Invert <= '0';
+
         r_A <= '0';
         r_B <= '0';
         WAIT FOR 2 ns;
-        REPORT "Test 1";
-        ASSERT r_Result = '0' SEVERITY failure;
+        ASSERT r_Result = '0' REPORT "Failed Test 1" SEVERITY failure;
 
         r_A <= '1';
         r_B <= '0';
         WAIT FOR 2 ns;
-        REPORT "Test 2";
-        ASSERT r_Result = '0' SEVERITY failure;
+        ASSERT r_Result = '0' REPORT "Failed Test 2" SEVERITY failure;
 
         r_A <= '0';
         r_B <= '1';
         WAIT FOR 2 ns;
-        REPORT "Test 3";
-        ASSERT r_Result = '0' SEVERITY failure;
+        ASSERT r_Result = '0' REPORT "Failed Test 3" SEVERITY failure;
 
         r_A <= '1';
         r_B <= '1';
         WAIT FOR 2 ns;
-        REPORT "Test 4";
-        ASSERT r_Result = '1' SEVERITY failure;
+        ASSERT r_Result = '1' REPORT "Failed Test 4" SEVERITY failure;
 
         ---------------------------- TESTING OR - Operation = OP_OR && A_Invert = 0 && B_Invert = 0 ----------------------------
         r_Operation <= OP_OR;
+        r_A_Invert <= '0';
+        r_B_Invert <= '0';
 
         r_A <= '0';
         r_B <= '0';
         WAIT FOR 2 ns;
-        REPORT "Test 5";
-        ASSERT r_Result = '0' SEVERITY failure;
+        ASSERT r_Result = '0' REPORT "Failed Test 5" SEVERITY failure;
 
         r_A <= '1';
         r_B <= '0';
         WAIT FOR 2 ns;
-        REPORT "Test 6";
-        ASSERT r_Result = '1' SEVERITY failure;
+        ASSERT r_Result = '1' REPORT "Failed Test 6" SEVERITY failure;
 
         r_A <= '0';
         r_B <= '1';
         WAIT FOR 2 ns;
-        REPORT "Test 7";
-        ASSERT r_Result = '1' SEVERITY failure;
+        ASSERT r_Result = '1' REPORT "Failed Test 7" SEVERITY failure;
 
         r_A <= '1';
         r_B <= '1';
         WAIT FOR 2 ns;
-        REPORT "Test 8";
-        ASSERT r_Result = '1' SEVERITY failure;
+        ASSERT r_Result = '1' REPORT "Failed Test 8" SEVERITY failure;
 
         ---------------------------- TESTING NAND - Operation = OP_OR && A_Invert = 1 && B_Invert = 1 ----------------------------
+        r_Operation <= OP_OR;
         r_A_Invert <= '1';
         r_B_Invert <= '1';
 
         r_A <= '0';
         r_B <= '0';
         WAIT FOR 2 ns;
-        REPORT "Test 1";
-        ASSERT r_Result = '1' SEVERITY failure;
+        ASSERT r_Result = '1' REPORT "Failed Test 9" SEVERITY failure;
 
         r_A <= '1';
         r_B <= '0';
         WAIT FOR 2 ns;
-        REPORT "Test 2";
-        ASSERT r_Result = '1' SEVERITY failure;
+        ASSERT r_Result = '1' REPORT "Failed Test 10" SEVERITY failure;
 
         r_A <= '0';
         r_B <= '1';
         WAIT FOR 2 ns;
-        REPORT "Test 3";
-        ASSERT r_Result = '1' SEVERITY failure;
+        ASSERT r_Result = '1' REPORT "Failed Test 11" SEVERITY failure;
 
         r_A <= '1';
         r_B <= '1';
         WAIT FOR 2 ns;
-        REPORT "Test 4";
-        ASSERT r_Result = '0' SEVERITY failure;
+        ASSERT r_Result = '0' REPORT "Failed Test 12" SEVERITY failure;
 
-        --#TODO: write a bunch more tests
+        ---------------------------- TESTING NOR - Operation = OP_AND && A_Invert = 1 && B_Invert = 1 ----------------------------
+        r_Operation <= OP_AND;
+        r_A_Invert <= '1';
+        r_B_Invert <= '1';
+
+        r_A <= '0';
+        r_B <= '0';
+        WAIT FOR 2 ns;
+        ASSERT r_Result = '1' REPORT "Failed Test 13" SEVERITY failure;
+
+        r_A <= '1';
+        r_B <= '0';
+        WAIT FOR 2 ns;
+        ASSERT r_Result = '0' REPORT "Failed Test 14" SEVERITY failure;
+
+        r_A <= '0';
+        r_B <= '1';
+        WAIT FOR 2 ns;
+        ASSERT r_Result = '0' REPORT "Failed Test 15" SEVERITY failure;
+
+        r_A <= '1';
+        r_B <= '1';
+        WAIT FOR 2 ns;
+        ASSERT r_Result = '0' REPORT "Failed Test 16" SEVERITY failure;
+
+        ---------------------------- TESTING Adder - Operation = OP_AND && A_Invert = 1 && B_Invert = 1 ----------------------------
+        r_Operation <= OP_Adder;
+        r_A_Invert <= '0';
+        r_B_Invert <= '0';
+
+        ---------------------------- Carry In == '0' ----------------------------
+        r_Carry_In <= '0';
+
+        r_A <= '0';
+        r_B <= '0';
+        WAIT FOR 2 ns;
+        ASSERT r_Result = '0' AND r_Carry_Out = '0' AND r_Set = '0' REPORT "Failed Test 17" SEVERITY failure;
+
+        r_A <= '1';
+        r_B <= '0';
+        WAIT FOR 2 ns;
+        ASSERT r_Result = '1' AND r_Carry_Out = '0' AND r_Set = '1' REPORT "Failed Test 18" SEVERITY failure;
+
+        r_A <= '0';
+        r_B <= '1';
+        WAIT FOR 2 ns;
+        ASSERT r_Result = '1' AND r_Carry_Out = '0' AND r_Set = '1' REPORT "Failed Test 19" SEVERITY failure;
+
+        r_A <= '1';
+        r_B <= '1';
+        WAIT FOR 2 ns;
+        ASSERT r_Result = '0' AND r_Carry_Out = '1' AND r_Set = '0' REPORT "Failed Test 20" SEVERITY failure;
+
+        ---------------------------- Carry In == '1' ----------------------------
+        r_Carry_In <= '1';
+
+        r_A <= '0';
+        r_B <= '0';
+        WAIT FOR 2 ns;
+        ASSERT r_Result = '1' AND r_Carry_Out = '0' AND r_Set = '1' REPORT "Failed Test 21" SEVERITY failure;
+
+        r_A <= '1';
+        r_B <= '0';
+        WAIT FOR 2 ns;
+        ASSERT r_Result = '0' AND r_Carry_Out = '1' AND r_Set = '0' REPORT "Failed Test 22" SEVERITY failure;
+
+        r_A <= '0';
+        r_B <= '1';
+        WAIT FOR 2 ns;
+        ASSERT r_Result = '0' AND r_Carry_Out = '1' AND r_Set = '0' REPORT "Failed Test 23" SEVERITY failure;
+
+        r_A <= '1';
+        r_B <= '1';
+        WAIT FOR 2 ns;
+        ASSERT r_Result = '1' AND r_Carry_Out = '1' AND r_Set = '1' REPORT "Failed Test 24" SEVERITY failure;
+
         finish;
     END PROCESS;
 END test;
